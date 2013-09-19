c6b5501ec2756ced9663bae4bf61440c1fe63b12
Jeremy Templier <templier.jeremy@gmail.com>
2013-09-19 00:57:15 -0700

    add paperclip and video storage using aws3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,4 +31,15 @@ ShakeReport::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # paperclip configuration
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+  
 end
