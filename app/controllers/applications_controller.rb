ba1b67cfd86882171c5f790c9716f4fe674a527f
Jeremy Templier <templier.jeremy@gmail.com>
2014-01-28 19:52:55 -0800

    add the current user as admin when create the application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationsController < ApplicationController
 
+
   def index
     @applications = current_user.applications
   end
@@ -8,8 +9,7 @@ class ApplicationsController < ApplicationController
     if current_user.is_super_admin
       application = Application.new(params[:application])
       if application.save
-        current_user.application_ids << application.id
-        current_user.save
+        ApplicationAccess.create!(application_id: application.id, user_id: current_user.id, role: ApplicationAccess::ROLE[:admin])
         redirect_to :back, notice: "Application created!"
       else
         redirect_to :back, alert: "An error occurred during the creation of the application"
