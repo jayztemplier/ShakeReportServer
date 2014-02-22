95cc49c8b31bc91fe6b92331b77344b5fd35ed3e
Jeremy Templier <templier.jeremy@gmail.com>
2014-02-21 20:28:17 -0800

    remove css bootstrap file to use the CDN

--- Gemfile
+++ Gemfile
@@ -23,7 +23,6 @@ group :assets do
   gem 'therubyracer', :platforms => :ruby
    gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
   gem 'uglifier', '>= 1.0.3'
-  gem 'twitter-bootstrap-rails', :git => 'git://github.com/seyhunak/twitter-bootstrap-rails.git', :branch => 'bootstrap3'
 end
 
 group :test do
--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module ShakeReport
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib)
 
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -41,6 +42,9 @@ module ShakeReport
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
