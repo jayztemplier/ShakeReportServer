c9015e1c3a31856280b6bfc0fd76bc6e3bb864e1
Jeremy Templier <templier.jeremy@gmail.com>
2014-02-22 22:35:43 -0800

    change the host for production (staging)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ ShakeReport::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'shakereporter.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'staging.shakereport.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
